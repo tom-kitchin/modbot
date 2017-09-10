70e62bf45ec983dcc2c519e49aba26dc139df5c4
Tom Kitchin <tkitchin@gmail.com>
2017-09-10 00:28:37 +0000

    Stop sending that reconnected message

--- src/modules/events/ready.rb
+++ src/modules/events/ready.rb
@@ -5,10 +5,6 @@ module Bot
       extend Discordrb::EventContainer
       ready do |event|
         event.bot.game = CONFIG.game
-
-        if CONFIG.audit_channel then
-          event.bot.send_message(CONFIG.audit_channel, "_reconnected_")
-        end
       end
     end
   end
