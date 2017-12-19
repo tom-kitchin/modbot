a14f20ea4a500e9a9743c0b8406200f2c2f4d92e
Tom Kitchin <tom.kitchin@create.net>
2017-12-19 16:26:52 +0000

    Add a debug mode

--- src/modules/events/pm.rb
+++ src/modules/events/pm.rb
@@ -19,24 +19,31 @@ module Bot
         end
       end
 
+      def get_owner_channel
+        target_channel_info_response = Discordrb::API::User.create_pm(event.bot.token, CONFIG.owner)
+        target_channel_info = JSON.parse(target_channel_info_response.body)
+        return target_channel_info['id']
+      end
+
       pm do |event|
         if event.content[0] == '!' then
           args = event.content.slice(1..-1).split
           event_name = args.slice!(0).lowercase.to_sym
           if ModbotPMCommands.respond_to? event_name then
+            if (CONFIG.debug) then event.bot.send_message(get_owner_channel(), "Trying to accept command #{event_name}") end
             ModbotPMCommands.send(event_name, event)
           else
             event.respond "Command `!#{event_name}` not recognised. If you were trying to reach the moderators with a message, just type it to me normally (without an ! on the beginning) :)"
           end
         else
-          if CONFIG.mod_channel then
+          if CONFIG.debug then
+            target_channel = get_owner_channel()
+          elsif CONFIG.mod_channel then
             target_channel = CONFIG.mod_channel
           else
             # If we don't have a mod channel, PM it to the bot owner and
             # also remind them to set a mod channel!
-            target_channel_info_response = Discordrb::API::User.create_pm(event.bot.token, CONFIG.owner)
-            target_channel_info = JSON.parse(target_channel_info_response.body)
-            target_channel = target_channel_info['id']
+            target_channel = get_owner_channel()
             event.bot.send_message(target_channel, "Hey, you should set my mod channel with set_mod_channel!")
           end
 
