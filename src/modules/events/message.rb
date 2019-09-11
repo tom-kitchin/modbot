af1004f885b3692d9a87814293daf337961f55d0
Tom Kitchin <tom.kitchin@inviqa.com>
2019-09-11 16:06:51 +0100

    Lazy fix for doubling message problem in PMs

--- src/modules/events/message.rb
+++ src/modules/events/message.rb
@@ -2,11 +2,16 @@ require_relative('../utils')
 
 module Bot
   module DiscordEvents
-    # This event is processed each time someone mentions the bot.
-    # It just describes the bot's basic functions.
+    # This event is processed on all messages.
+    # Honestly it triggers IN ADDITION TO matching events like mention or PM in the relevant
+    # contexts. There are bugs here, one day I may even fix them.
+    # Anyway if you see doubling responses from modbot, check here first.
     module Message
       extend Discordrb::EventContainer
       message(start_with: CONFIG.prefix) do |event|
+        # This event now also triggers in PMs, so bin _that_ off.
+        return unless event.server
+
         args = event.content.downcase.strip.slice(1..-1).split
         command_name = args.slice!(0).downcase.to_sym
         if ModbotCommands.respond_to? command_name
